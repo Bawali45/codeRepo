25f1cfbadc2a4fcfac599c9473f04f4903dd5ebe
Bawali45 <127670277+Bawali45@users.noreply.github.com>
2023-03-27 23:44:34 +0530

    Add files via upload

---
+++ celling_of_a_number_in_an_array.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+//  Celling number in the array  = Samallest element in the array which is grater then taget or equal to target.
+int celling_number(int arr[],int size,int val)
+{
+    int start = 0;
+    int end = size-1;
+    int mid=start + (end-start)/2;
+    while(start<=end)
+    {
+        if(val>arr[mid])
+        {
+         start = mid+1;
+        }
+        else if(val<arr[mid])
+        {
+           end =  mid-1;
+        }
+        else{
+            return mid;
+        }
+        mid = start + (end-start)/2;
+    }
+    return start;
+}
+
+int main()
+{
+int arr[] = {1,2,3,4,7,8,9};
+int n = 7;
+int key;
+cin>>key;
+int res = celling_number(arr,n,key);
+cout<<res<<endl;
+return 0;
+}
